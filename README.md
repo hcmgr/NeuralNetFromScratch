4a3be9d1cefd11d8361e06ab614d1a6b7e498556
hcmgr <harryrackemann@gmail.com>
2023-06-23 13:09:30 +1000

    added back update_mini_batch

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # NEURAL NETWORKS FROM SCRATCH #
+This is my attempt at implementing hand-written digit detection from sratch. I implemented my own linear algebra library, which was MUCH slower than NumPy, but was a fun challenge nonetheless.
+
+Credit to Michael Nielson's 'Neural Networks and Deep Learning' for inspiring this project
+
+
+
+
--- network.py
+++ network.py
@@ -31,85 +31,94 @@ def sigmoid_prime(z):
 
 class Network():
 
-	def __init__(self, layerSizes):
-		"""
-		A tuple containing the sizes of each of the layers
-		eg: a network with 10 inputs neurons, 30 hidden neurons
-		and 5 output neurons, layerStrucutre = (10, 30, 5) 
-		"""
-		self.layerSizes = layerSizes
+    def __init__(self, layerSizes):
+        """
+        A tuple containing the sizes of each of the layers
+        eg: a network with 10 inputs neurons, 30 hidden neurons
+        and 5 output neurons, layerStrucutre = (10, 30, 5) 
+        """
+        self.layerSizes = layerSizes
 
-		"""
-		The weights are stored as a list of matrices (one matrix per layer).
-		This is an nxm matrix where n is the number of neurons in the lth 
-		layer and m is the number of neurons in the l-1th layer.
+        """
+        The weights are stored as a list of matrices (one matrix per layer).
+        This is an nxm matrix where n is the number of neurons in the lth 
+        layer and m is the number of neurons in the l-1th layer.
 
-		eg: in a (10, 30, 5) network, the hidden layers weights is
-		represented by a 30 x 10 matrix
+        eg: in a (10, 30, 5) network, the hidden layers weights is
+        represented by a 30 x 10 matrix
 
-		NOTE: each matrix is an instance of the Matrix class in
-		LinearAlgLibrary.py (the linear algebra library built for this project)
-		
-		NOTE: we can see that the jth row of the matrix gives us the 
-		weights connected to the jth neuron in the lth layer
-		"""
-		self.weights = self.initWeights()
-		#print("first set of weights: ", self.weights[0])
+        NOTE: each matrix is an instance of the Matrix class in
+        LinearAlgLibrary.py (the linear algebra library built for this project)
+        
+        NOTE: we can see that the jth row of the matrix gives us the 
+        weights connected to the jth neuron in the lth layer
+        """
+        self.weights = self.initWeights()
+        #print("first set of weights: ", self.weights[0])
 
-		"""
-		The biases are stored as a list of vectors (one vector per layer).
-		This is a nx1 vector where n is the number of neurons in the 
-		lth layer. 
-		"""
-		self.biases = self.initBiases()
-		#print("first set of biases: ", self.biases[0])
+        """
+        The biases are stored as a list of vectors (one vector per layer).
+        This is a nx1 vector where n is the number of neurons in the 
+        lth layer. 
+        """
+        self.biases = self.initBiases()
+        #print("first set of biases: ", self.biases[0])
 
-	def initWeights(self):
-		weights = []
-		for x, y in zip(self.layerSizes[:-1], self.layerSizes[1:]):
-			weights.append(Matrix.generateMatrixRand(y, x))
-		return weights
+    def initWeights(self):
+        weights = []
+        for x, y in zip(self.layerSizes[:-1], self.layerSizes[1:]):
+            weights.append(Matrix.generateMatrixRand(y, x))
+        return weights
 
-	def initBiases(self):
-		biases = []
-		for x in self.layerSizes[1:]:
-			biases.append(Matrix.generateMatrixRand(x, 1))
-		return biases
-
-
-	def gradDescent(self, trainingData, numEpochs, miniSize, learnRate, testData=None):
-		for i in range(numEpochs):
-			miniBatches = self.createMiniBatches(trainingData, miniSize)
-			for miniBatch in miniBatches:
-				##calculate grad C
-				##change weights and biases accordingly
-				pass
-			##output results of epoch
-			result = self.test(testImages, testLabels)
-			print(f"Epoch {i}: {result}/{len(testImages)}\n")
-
-	"""
-	Returns a list of lists, whereby each list is a mini-batch of image matrices
-	"""
-	def createMiniBatches(self, trainingData, size):
-		return [[trainingData[j] for j in range(i, i + size)] for i in range(0, len(trainingData), size)]
+    def initBiases(self):
+        biases = []
+        for x in self.layerSizes[1:]:
+            biases.append(Matrix.generateMatrixRand(x, 1))
+        return biases
 
 
-	"""
-	Given an input vector (x), feed forward through the network 
-	to calculate the output vector (a)
+    def gradDescent(self, trainingData, numEpochs, miniSize, learnRate, testData=None):
+        for i in range(numEpochs):
+            miniBatches = self.createMiniBatches(trainingData, miniSize)
+            for miniBatch in miniBatches:
+                self.update_mini_batch(mini_batch, eta)
+            result = self.test(testImages, testLabels)
+            print(f"Epoch {i}: {result}/{len(testImages)}\n")
 
-	params:
-		x (Matrix): n x 1 input vector, where n is number of input neurons
-	"""
-	def feedForward(self, x):
-		aPrev = x
-		for w, b in zip(self.weights, self.biases):
-			##formula for the activation of a neuron: ((w * a) + b)##
-			z = w.mult(aPrev).add(b)
-			aPrev = z.apply(sigmoid)
-		return aPrev
+    def update_mini_batch(self, mini_batch, eta):
+        n_b = [np.zeros(b.shape) for b in self.biases]
+        n_w = [np.zeros(w.shape) for w in self.weights]
+        for x, y in mini_batch:
+            d_n_b, d_n_w = self.backprop(x, y)
+            n_b = [new_b+dnb for new_b, dnb in zip(n_b, d_n_b)]
+            n_w = [new_w+dnw for new_w, dnw in zip(n_w, d_n_w)]
+        self.weights = [w-(eta/len(mini_batch))*new_w
+                        for w, new_w in zip(self.weights, n_w)]
+        self.biases = [b-(eta/len(mini_batch))*new_b
+                       for b, new_b in zip(self.biases, n_b)]
 
-	def test(self, testImages, testLabels):
-		results = [(Matrix.maxElIndex(self.feedForward(x)), y) for x, y in zip(testImages, testLabels)]
-		return sum(int(x == y) for x, y in results)
+    """
+    Returns a list of lists, whereby each list is a mini-batch of image matrices
+    """
+    def createMiniBatches(self, trainingData, size):
+        return [[trainingData[j] for j in range(i, i + size)] for i in range(0, len(trainingData), size)]
+
+
+    """
+    Given an input vector (x), feed forward through the network 
+    to calculate the output vector (a)
+
+    params:
+        x (Matrix): n x 1 input vector, where n is number of input neurons
+    """
+    def feedForward(self, x):
+        aPrev = x
+        for w, b in zip(self.weights, self.biases):
+            ##formula for the activation of a neuron: ((w * a) + b)##
+            z = w.mult(aPrev).add(b)
+            aPrev = z.apply(sigmoid)
+        return aPrev
+
+    def test(self, testImages, testLabels):
+        results = [(Matrix.maxElIndex(self.feedForward(x)), y) for x, y in zip(testImages, testLabels)]
+        return sum(int(x == y) for x, y in results)
